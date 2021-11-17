32e68edaa24ae322eb5ba6c31a743de330150e0d
hosseinghaedi1999@yahoo.com <Admin5525181>
2021-11-17 18:01:23 +0400

    adding JS and Connecting

---
+++ lessons.js
@@ -0,0 +1,91 @@
+let lessons = [
+    {
+    "id":2001,
+    "subject": "Math Class",
+    "location" : "Dubai",
+    "price":6000,
+    "image": "assets/math.jpg",
+    "numberOfsits": 10,
+    },
+ 
+    {
+        "id":2002,
+        "subject": "Science Class",
+        "location" : "Dubai",
+        "price":6500,
+        "image": "assets/math.jpg",
+        "numberOfsits": 8,
+    },
+
+    {
+        "id":2003,
+        "subject": "Dancing",
+        "location" : "Dubai",
+        "price":4500,
+        "image": "assets/math.jpg",
+        "numberOfsits": 10,
+    },
+
+    {
+        "id":2004,
+        "subject": "Karate",
+        "location" : "Dubai",
+        "price":5500,
+        "image": "assets/math.jpg",
+        "numberOfsits": 6,
+    },
+
+    {
+        "id":2005,
+        "subject": "Singing",
+        "location" : "Dubai",
+        "price":8000,
+        "image": "assets/math.jpg",
+        "numberOfsits": 6,
+    },
+
+    {
+        "id":2006,
+        "subject": "Arabic lessons",
+        "location" : "Dubai",
+        "price":4500,
+        "image": "assets/math.jpg",
+        "numberOfsits": 8,
+    },
+    {
+        "id":2007,
+        "subject": "Judo",
+        "location" : "Dubai",
+        "price":4500,
+        "image": "assets/math.jpg",
+        "numberOfsits": 9,
+    },
+
+    {
+        "id":2008,
+        "subject": "Singing",
+        "location" : "Dubai",
+        "price":3500,
+        "image": "assets/math.jpg",
+        "numberOfsits": 5,
+    },
+
+    {
+        "id":2009,
+        "subject": "Football",
+        "location" : "Dubai",
+        "price":8900,
+        "image": "assets/math.jpg",
+        "numberOfsits": 6,
+    },
+
+    {
+        "id":2010,
+        "subject": "Piano",
+        "location" : "Dubai",
+        "price":4600,
+        "image": "assets/math.jpg",
+        "numberOfsits": 6,
+    }
+
+]
